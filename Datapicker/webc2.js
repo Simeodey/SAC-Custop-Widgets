f4d68c6760174e14a9b07a420bfa20b777a54e5c
Simeodey <62400094+Simeodey@users.noreply.github.com>
2020-05-28 07:09:17 +0300

    Add files via upload

---
+++ Datapicker/webc2.js
@@ -0,0 +1,70 @@
+(function () {
+    let tmpl = document.createElement('template');
+    tmpl.innerHTML = `
+    <style type="text/css">	
+    body {
+
+    }
+    </style>       
+`;
+
+    class DatePicker extends HTMLElement {
+        constructor() {
+            super();
+            this.init();
+        }
+        
+
+        init() {
+            if (this.children.length === 2) return; //constructor called during drag+drop
+            if (!this.querySelector("link")) {
+                this.appendChild(tmpl.content.cloneNode(true));
+            }
+            var ctor = sap.m.DatePicker;
+            if (this._enablerange) { ctor = sap.m.DateRangeSelection; }
+            this.DP = new ctor({
+                change: function () {
+                    this.fireChanged();
+                    this.dispatchEvent(new Event("onChange"));
+                }.bind(this)
+            }).addStyleClass("datePicker");
+            this.DP.placeAt(this);
+        }
+
+        fireChanged() {
+            var properties = { dateVal: this.DP.getDateValue() };
+            if (this._enablerange) { properties.secondDateVal = this.DP.getSecondDateValue(); }
+            this.dispatchEvent(new CustomEvent("propertiesChanged", {
+                detail: {
+                    properties: properties
+                }
+            }));
+        }
+
+        set dateVal(value) {
+            if (value == undefined || !this.DP) return;
+            if (typeof (value) === "string") value = new Date(value);
+            this.DP.setDateValue(value);
+        }
+
+        set secondDateVal(value) {
+            if (value == undefined || !this.DP || !this._enablerange) return;
+            if (typeof (value) === "string") value = new Date(value);
+            this.DP.setSecondDateValue(value);
+        }
+
+        set format(value) {
+            if (!this.DP) return;
+            this.DP.setDisplayFormat(value);
+        }
+
+        set enablerange(value) {
+            if (value == undefined || !this.DP) return;
+            this._enablerange = value;
+            this.DP.destroy();
+            this.init();
+        }
+    }
+
+    customElements.define('nkappler-date-picker', DatePicker);
+})();
