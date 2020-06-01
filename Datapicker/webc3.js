3c4f1b66d0ab2bf1fbb08af48fcaf6045255cac1
Simeodey <62400094+Simeodey@users.noreply.github.com>
2020-06-01 15:41:55 +0300

    Update webc3.js

--- Datapicker/webc3.js
+++ Datapicker/webc3.js
@@ -1,4 +1,5 @@
 (function () {
+	let shadowRoot;
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `    
 `;
@@ -6,10 +7,10 @@
     class DatePicker extends HTMLElement {
         constructor() {
             super();
-            _shadowRoot = this.attachShadow({
+            shadowRoot = this.attachShadow({
                 mode: "open"
             });
-            _shadowRoot.appendChild(tmpl.content.cloneNode(true));
+           shadowRoot.appendChild(tmpl.content.cloneNode(true));
             
         }
         
