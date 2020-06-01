728dde8514bb8e987e8190430b9e7d05eafbaee2
Simeodey <62400094+Simeodey@users.noreply.github.com>
2020-06-01 16:09:23 +0300

    Update webc3.js

--- Datapicker/webc3.js
+++ Datapicker/webc3.js
@@ -1,5 +1,4 @@
 (function () {
-	let shadowRoot;
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `    
 `;
@@ -7,49 +6,67 @@
     class DatePicker extends HTMLElement {
         constructor() {
             super();
-            shadowRoot = this.attachShadow({
+            _shadowRoot = this.attachShadow({
                 mode: "open"
             });
-           shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            _shadowRoot.appendChild(tmpl.content.cloneNode(true));
             
         }
         
+          //Fired when the widget is added to the html DOM of the page
           connectedCallback(){
             this.init(); 
         }
 
-         
+         //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
         disconnectedCallback(){
         
         }
 
-         
+         //When the custom widget is updated, the Custom Widget SDK framework executes this function first
 		onCustomWidgetBeforeUpdate(oChangedProperties) {
 
 		}
 
-       
+        //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
 		onCustomWidgetAfterUpdate(oChangedProperties) {
 
                 this.init();
             
         }
         
-        
+        //When the custom widget is removed from the canvas or the analytic application is closed
         onCustomWidgetDestroy(){
         
         }
     
         init() {
+         
             var dpicker = sap.m.DatePicker;
-            this.DP = new dpicker().addStyleClass("datePicker");
+            this.DP = new dpicker({
+                change: function () {
+                    this.fireChanged();
+                    this.dispatchEvent(new Event("onChange"));
+                }.bind(this)
+            }).addStyleClass("datePicker");
             this.DP.placeAt(this);
-	        console.log(this);
         }
 
-        
+        fireChanged() {
+            var properties = { date: this.DP.getDateValue(),
+                             format: 'yy-mm-dd' };
+            this.dispatchEvent(new CustomEvent("propertiesChanged", {
+                detail: {
+                    properties: properties
+                }
+            }));
+        }
 
- 
+        set date(value) {
+            if (value == undefined || !this.DP) return;
+            if (typeof (value) === "string") value = new Date(value);
+            this.DP.setDateValue(value);
+        }
     }
 
     customElements.define('ui5-datepicker', DatePicker);
