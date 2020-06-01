1430692c81fc4a1868d4f6e893128d32a483df6f
Simeodey <62400094+Simeodey@users.noreply.github.com>
2020-06-01 14:47:07 +0300

    Add files via upload

---
+++ Datapicker/webc3.js
@@ -0,0 +1,57 @@
+(function () {
+    let tmpl = document.createElement('template');
+    tmpl.innerHTML = `   <style>	
+    body {
+
+    }
+    </style>  
+`;
+
+    class DatePicker extends HTMLElement {
+        constructor() {
+            super();
+            _shadowRoot = this.attachShadow({
+                mode: "open"
+            });
+            _shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            this.addEventListener("click", event => {
+                console.log('click');
+                var event = new Event("onClick");
+                this.dispatchEvent(event);})
+        }
+        
+
+        init() {
+            if (this.children.length === 2) return;
+            if (!this.querySelector("link")) {
+                this.appendChild(tmpl.content.cloneNode(true));
+            }
+            var dpicker = sap.m.DatePicker;
+            this.DP = new dpicker({
+                change: function () {
+                    this.fireChanged();
+                    this.dispatchEvent(new Event("onChange"));
+                }.bind(this)
+            }).addStyleClass("datePicker");
+            this.DP.placeAt(this);
+        }
+
+        fireChanged() {
+            var properties = { date: this.DP.getDateValue(),
+                             format: 'yy-mm-dd' };
+            this.dispatchEvent(new CustomEvent("propertiesChanged", {
+                detail: {
+                    properties: properties
+                }
+            }));
+        }
+
+        set date(value) {
+            if (value == undefined || !this.DP) return;
+            if (typeof (value) === "string") value = new Date(value);
+            this.DP.setDateValue(value);
+        }
+    }
+
+    customElements.define('ui5-datepicker', DatePicker);
+})();
