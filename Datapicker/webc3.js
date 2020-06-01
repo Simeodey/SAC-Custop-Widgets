5f36cc78c885bca9298a276b80c12bcfe795095c
Simeodey <62400094+Simeodey@users.noreply.github.com>
2020-06-01 15:54:09 +0300

    Update webc3.js

--- Datapicker/webc3.js
+++ Datapicker/webc3.js
@@ -41,32 +41,14 @@
         }
     
         init() {
-            if (this.children.length === 2) return;
             var dpicker = sap.m.DatePicker;
-            this.DP = new dpicker({
-                change: function () {
-                    this.fireChanged();
-                    this.dispatchEvent(new Event("onChange"));
-                }.bind(this)
-            }).addStyleClass("datePicker");
+            this.DP = new dpicker().addStyleClass("datePicker");
             this.DP.placeAt(this);
         }
 
-        fireChanged() {
-            var properties = { date: this.DP.getDateValue(),
-                             format: 'yy-mm-dd' };
-            this.dispatchEvent(new CustomEvent("propertiesChanged", {
-                detail: {
-                    properties: properties
-                }
-            }));
-        }
+        
 
-        set date(value) {
-            if (value == undefined || !this.DP) return;
-            if (typeof (value) === "string") value = new Date(value);
-            this.DP.setDateValue(value);
-        }
+ 
     }
 
     customElements.define('ui5-datepicker', DatePicker);
