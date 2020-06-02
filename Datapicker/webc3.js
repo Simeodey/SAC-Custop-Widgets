54efd8207da5d11496958258c002b3175ce5988f
Simeodey <62400094+Simeodey@users.noreply.github.com>
2020-06-03 00:24:32 +0300

    Update webc3.js

--- Datapicker/webc3.js
+++ Datapicker/webc3.js
@@ -1,44 +1,50 @@
 (function () {
     let tmpl = document.createElement('template');
-    tmpl.innerHTML = `    
+    tmpl.innerHTML = `
+    <style>
+    <div id="ui5_content" name="ui5_content">
+         <slot name="content"></slot>
+        </div>  
+        </style>  
 `;
+let content = document.createElement('div');
+content.slot = "content";
 
     class DatePicker extends HTMLElement {
         constructor() {
             super();
-            let _shadowRoot = this.attachShadow({
+            _shadowRoot = this.attachShadow({
                 mode: "open"
             });
             _shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            this._firstConnection = false;
             
         }
         
-          //Fired when the widget is added to the html DOM of the page
           connectedCallback(){
+            this._firstConnection = true;
             this.init(); 
         }
 
-         //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
         disconnectedCallback(){
         
         }
 
-         //When the custom widget is updated, the Custom Widget SDK framework executes this function first
 		onCustomWidgetBeforeUpdate(oChangedProperties) {
 
 		}
 
-        //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
 		onCustomWidgetAfterUpdate(oChangedProperties) {
-
+            if (this._firstConnection){
                 this.init();
-            
+            }
         }
         
-        //When the custom widget is removed from the canvas or the analytic application is closed
         onCustomWidgetDestroy(){
         
         }
+
+        
     
         init() {
          
@@ -49,7 +55,7 @@
                     this.dispatchEvent(new Event("onChange"));
                 }.bind(this)
             }).addStyleClass("datePicker");
-            this.DP.placeAt(this);
+            this.DP.placeAt(content);
         }
 
         fireChanged() {
