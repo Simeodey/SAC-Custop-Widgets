38ce87bfd3843f8fcc5c9fe0413ce57aec1b0077
Simeodey <62400094+Simeodey@users.noreply.github.com>
2020-06-03 11:29:06 +0300

    Update webc2.js

--- Datapicker/webc2.js
+++ Datapicker/webc2.js
@@ -1,71 +1,32 @@
 (function () {
     let tmpl = document.createElement('template');
-    tmpl.innerHTML = `
-    <div id="ui5_content" name="ui5_content">
-         <slot name="content"></slot>
-        </div>  
+    tmpl.innerHTML = `       
 `;
-
 
     class DatePicker extends HTMLElement {
         constructor() {
             super();
-            let _shadowRoot = this.attachShadow({
-                mode: "open"
-            });
-            _shadowRoot.appendChild(tmpl.content.cloneNode(true));
-            this._firstConnection = false;
-             this.addEventListener("onChange", event => {
-                console.log('Changed');
-            });
-            
+            this.init();
         }
         
-          connectedCallback(){
-            this._firstConnection = true;
-            this.load(); 
-        }
 
-        disconnectedCallback(){
-        
-        }
-
-		onCustomWidgetBeforeUpdate(oChangedProperties) {
-
-		}
-
-		onCustomWidgetAfterUpdate(oChangedProperties) {
-            if (this._firstConnection){
-                this.load();
+        init() {
+            if (this.children.length === 2) return;
+            if (!this.querySelector("link")) {
+                this.appendChild(tmpl.content.cloneNode(true));
             }
-        }
-        
-        onCustomWidgetDestroy(){
-        
-        }
-
-        
-    
-        load() {
-         let content = document.createElement('div');
-	content.slot = "content";
-	this.appendChild(content);
             var dpicker = sap.m.DatePicker;
             this.DP = new dpicker({
                 change: function () {
-                    this.changed();
+                    this.fireChanged();
                     this.dispatchEvent(new Event("onChange"));
                 }.bind(this)
             }).addStyleClass("datePicker");
-		this.DP.setPlaceholder("MMM d y");
-		//this.DP.setDisplayFormatType("dd.MM.YYYY");
-            this.DP.placeAt(content);
-
+            this.DP.placeAt(this);
         }
 
-        changed() {
-            var properties = { date: this.DP.getDateValue(),
-                             format: 'yy-mm-dd' };
+        fireChanged() {
+            var properties = { date: this.DP.getDateValue() };
             this.dispatchEvent(new CustomEvent("propertiesChanged", {
                 detail: {
                     properties: properties
@@ -78,6 +39,7 @@
             if (typeof (value) === "string") value = new Date(value);
             this.DP.setDateValue(value);
         }
+
     }
 
     customElements.define('ui5-datepicker', DatePicker);
